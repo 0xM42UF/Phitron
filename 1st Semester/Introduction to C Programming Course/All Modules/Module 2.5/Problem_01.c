4f0937e12226a8e9563f94ea7fe2487bbc92c868
Gazi Maruf Rahman <69670001+0xM42UF@users.noreply.github.com>
2022-11-02 09:34:05 -0700

    Delete 1st Semester/Introduction to C Programming Course/All Modules directory

--- 1st Semester/Introduction to C Programming Course/All Modules/Module 2.5/Problem_01.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-
-    printf("Hi there /\. \t I hope you are doing great and practicing well.\n Now this is a simple task you need to do. \t Just care for the indentation.\n Show 100% of sincerity.");
-
-    return 0;
-}
